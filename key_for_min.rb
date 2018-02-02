7e71127a3040fc9d97979e395432702562757654
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-02-02 18:59:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  ikea = {:chair => 25, :table => 35, :mattress => 500}
+  key_for_min_value(ikea)
+end
 
 end
